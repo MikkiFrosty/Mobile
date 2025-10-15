ac66d5a1a8eae20da805936c1b3e62681582388e
SKropotova <Kropotova.S.M@yandex.ru>
2025-10-16 00:54:38 +0300

    fix conftest

--- conftest.py
+++ conftest.py
@@ -1,81 +1,86 @@
-import os
 
-import allure
+import os
 import pytest
+import allure
 import requests
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+from dotenv import load_dotenv
 
-from config import load_config
-
-def _attach_bs_video(session_id: str):
-    user = os.getenv('BROWSERSTACK_USERNAME')
-    key = os.getenv('BROWSERSTACK_ACCESS_KEY')
-    if not (user and key):
-        return
-    try:
-        r = requests.get(
-            f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
-            auth=(user, key),
-            timeout=20
-        )
-        video_url = r.json().get('automation_session', {}).get('video_url')
-        if video_url:
-            html = f'<html><body><video width="100%" controls autoplay><source src="{video_url}" type="video/mp4"></video></body></html>'
-            allure.attach(html, name='BrowserStack video', attachment_type=allure.attachment_type.HTML)
-    except Exception:
-        pass
-
-def pytest_addoption(parser):
-    parser.addoption("--env", action="store", default=os.getenv("ENV", "bstack"), help="local | bstack")
-
-@pytest.fixture(scope="session")
-def cfg(request):
-    env = request.config.getoption("--env")
-    return load_config(env)
-
-@pytest.fixture(scope='function')
-def mobile_driver(cfg):
-    if cfg.env == "bstack":
-        caps = {
-            "platformName": "android",
-            "appium:automationName": "UiAutomator2",
-            "app": cfg.app_id,
-            "bstack:options": {
-                "projectName": "Mobile Wikipedia",
-                "buildName": "mobile-wiki",
-                "sessionName": "pytest",
-                "deviceName": cfg.device,
-                "osVersion": cfg.os_version,
-                "debug": True,
-                "networkLogs": True,
-                "video": True,
-                "userName": cfg.username,
-                "accessKey": cfg.access_key
-            }
-        }
-        options = UiAutomator2Options().load_capabilities(caps)
-        driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+def load_env():
+    env_context = os.getenv("ENV_CONTEXT")
+    if env_context:
+        env_file = f".env.{env_context}"
+        if os.path.exists(env_file):
+            load_dotenv(env_file)
+        else:
+            load_dotenv(".env")
     else:
-        caps = {
+        load_dotenv(".env")
+
+load_env()
+
+@pytest.fixture(scope="function")
+def driver():
+    env_type = os.getenv("ENV", "local")
+
+    if env_type == "bstack":
+        desired_caps = {
             "platformName": "Android",
-            "appium:automationName": "UiAutomator2",
-            "appium:appPackage": cfg.app_package,
-            "appium:appActivity": cfg.app_activity,
-            "appium:noReset": True,
-            "appium:newCommandTimeout": 120
+            "deviceName": os.getenv("DEVICE_NAME"),
+            "os_version": os.getenv("OS_VERSION"),
+            "app": os.getenv("APP"),
+            "automationName": "UiAutomator2",
+            "project": "QA-GURU Mobile",
+            "build": os.getenv("BS_BUILD_NAME", "Default Build"),
+            "name": os.getenv("BS_SESSION_NAME", "Pytest Session"),
+            "autoGrantPermissions": True,
+            "newCommandTimeout": 120
         }
-        options = UiAutomator2Options().load_capabilities(caps)
-        driver = webdriver.Remote(cfg.appium_server, options=options)
+
+        options = UiAutomator2Options().load_capabilities(desired_caps)
+        driver = webdriver.Remote(
+            command_executor="http://hub.browserstack.com/wd/hub",
+            options=options
+        )
+    else:
+        desired_caps = {
+            "platformName": "Android",
+            "automationName": "UiAutomator2",
+            "deviceName": os.getenv("ANDROID_DEVICE_NAME", "Android Emulator"),
+            "platformVersion": os.getenv("ANDROID_PLATFORM_VERSION", "13"),
+            "appPackage": os.getenv("ANDROID_APP_PACKAGE", "org.wikipedia.alpha"),
+            "appActivity": os.getenv("ANDROID_APP_ACTIVITY", "org.wikipedia.main.MainActivity"),
+            "autoGrantPermissions": True,
+            "newCommandTimeout": 120,
+        }
+        options = UiAutomator2Options().load_capabilities(desired_caps)
+        appium_server = os.getenv("APPIUM_SERVER", "http://127.0.0.1:4723/wd/hub")
+        driver = webdriver.Remote(appium_server, options=options)
 
     yield driver
 
+    # Attachments after test
     try:
         png = driver.get_screenshot_as_png()
-        allure.attach(png, name="last screenshot", attachment_type=allure.attachment_type.PNG)
+        allure.attach(png, name="screenshot", attachment_type=allure.attachment_type.PNG)
         src = driver.page_source
         allure.attach(src, name="page source", attachment_type=allure.attachment_type.XML)
-        if cfg.env == "bstack":
-            _attach_bs_video(driver.session_id)
+
+        if env_type == "bstack":
+            user = os.getenv("BROWSERSTACK_USERNAME")
+            key = os.getenv("BROWSERSTACK_ACCESS_KEY")
+            if user and key:
+                session_id = driver.session_id
+                url = f"https://api.browserstack.com/app-automate/sessions/{session_id}.json"
+                try:
+                    resp = requests.get(url, auth=(user, key), timeout=10)
+                    if resp.status_code == 200:
+                        data = resp.json().get("automation_session", {})
+                        video_url = data.get("video_url")
+                        if video_url:
+                            allure.attach(video_url, name="BrowserStack video", attachment_type=allure.attachment_type.URI_LIST)
+                except Exception as e:
+                    allure.attach(str(e), name="bs_video_error", attachment_type=allure.attachment_type.TEXT)
     finally:
         driver.quit()
